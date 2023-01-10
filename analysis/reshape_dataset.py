c1f3ac33ef8b346bc906d51e298c439c467c5629
Iain Dillingham <iain@dillingham.me.uk>
2023-01-10 08:23:05 +0000

    Move Pandas reshape out of `main`
    We will compare the Pandas reshape with an PyArrow reshape. To make this
    easier, we Move the Pandas reshape out of `main`.

--- analysis/reshape_dataset.py
+++ analysis/reshape_dataset.py
@@ -2,11 +2,8 @@ import pandas
 
 from analysis.utils import OUTPUT_DIR
 
-f_in = OUTPUT_DIR / "dataset_wide.arrow"
-f_out = OUTPUT_DIR / "dataset_long.arrow"
 
-
-def main():
+def reshape_pandas(f_in, f_out):
     dataset_wide = pandas.read_feather(f_in)
     dataset_long = pandas.wide_to_long(
         dataset_wide,
@@ -18,6 +15,12 @@ def main():
     # dataset_long has a MultiIndex; feather only supports a RangeIndex. So, we reset
     # before we write.
     dataset_long.reset_index().to_feather(f_out)
+
+
+def main():
+    f_in = OUTPUT_DIR / "dataset_wide.arrow"
+    f_out = OUTPUT_DIR / "dataset_long.arrow"
+    reshape_pandas(f_in, f_out)
 
 
 if __name__ == "__main__":
