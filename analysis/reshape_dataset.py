f6e397e3c5997d86060e32d70aa1054686053c5a
Iain Dillingham <iain@dillingham.me.uk>
2023-01-10 08:38:17 +0000

    Add PyArrow reshape
    We can compare Pandas reshape with PyArrow reshape by switching between
    `reshape_pandas` and `reshape_pyarrow` in `main`.

--- analysis/reshape_dataset.py
+++ analysis/reshape_dataset.py
@@ -1,4 +1,8 @@
+import re
+
+import more_itertools
 import pandas
+import pyarrow
 
 from analysis.utils import OUTPUT_DIR
 
@@ -17,10 +21,37 @@ def reshape_pandas(f_in, f_out):
     dataset_long.reset_index().to_feather(f_out)
 
 
+def strip_suffix(s):
+    return re.match(r"(\w+)_\d+", s).group(1)
+
+
+def reshape_pyarrow(f_in, f_out):
+    with pyarrow.memory_map(str(f_in), "rb") as memory_mapped_file:
+        table_wide = pyarrow.ipc.open_file(memory_mapped_file).read_all()
+
+    index_cols = table_wide.column_names[:3]
+    stack_cols = table_wide.column_names[3:]
+
+    table_long_groups = []
+    for stack_cols_group in more_itertools.grouper(stack_cols, 2):
+        stack_cols_group = list(stack_cols_group)
+        arrays = list(table_wide.select(index_cols + stack_cols_group))
+        names = index_cols + [strip_suffix(x) for x in stack_cols_group]
+        table_long_group = pyarrow.Table.from_arrays(arrays, names)
+        table_long_groups.append(table_long_group)
+
+    table_long = pyarrow.concat_tables(table_long_groups)
+
+    with pyarrow.OSFile(str(f_out), "wb") as sink:
+        with pyarrow.ipc.new_file(sink, table_long.schema) as writer:
+            for batch in table_long.to_batches():
+                writer.write(batch)
+
+
 def main():
     f_in = OUTPUT_DIR / "dataset_wide.arrow"
     f_out = OUTPUT_DIR / "dataset_long.arrow"
-    reshape_pandas(f_in, f_out)
+    reshape_pyarrow(f_in, f_out)
 
 
 if __name__ == "__main__":
