b73a9f2e1443b7c16b0fad28e9c2f54e1933d8ae
Iain Dillingham <iain@dillingham.me.uk>
2023-01-17 10:25:41 +0000

    Add new group of null elements
    Although there is a group in the input table for a third appointment,
    Patients 1, 2, and 3 don't have one. Consequently, this group comprises
    null elements. Because `stacker` doesn't filter out null elements, this
    group is present in the output table.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -25,6 +25,8 @@ def test_stacker():
             pyarrow.array([1, 1, 1]),
             pyarrow.array(["2022-02-02", None, "2022-02-02"]),
             pyarrow.array([2, None, 2]),
+            pyarrow.array([None, None, None]),
+            pyarrow.array([None, None, None]),
         ],
         names=[
             "patient_id",
@@ -33,6 +35,8 @@ def test_stacker():
             "lead_time_in_days_1",
             "booked_date_2",
             "lead_time_in_days_2",
+            "booked_date_3",
+            "lead_time_in_days_3",
         ],
     )
 
@@ -40,7 +44,7 @@ def test_stacker():
     stack = reshape_dataset.stacker(
         table_wide, ["patient_id", "practice"], 2, "appointment_num"
     )
-    sub_stack_1, sub_stack_2 = list(stack)
+    sub_stack_1, sub_stack_2, sub_stack_3 = list(stack)
 
     # assert
     assert sub_stack_1.column_names == [
@@ -62,3 +66,13 @@ def test_stacker():
     ]
     assert sub_stack_2["patient_id"].to_pylist() == [1, 2, 3]
     assert sub_stack_2["lead_time_in_days"].to_pylist() == [2, None, 2]
+
+    assert sub_stack_3.column_names == [
+        "patient_id",
+        "practice",
+        "booked_date",
+        "lead_time_in_days",
+        "appointment_num",
+    ]
+    assert sub_stack_3["patient_id"].to_pylist() == [1, 2, 3]
+    assert sub_stack_3["lead_time_in_days"].to_pylist() == [None, None, None]
