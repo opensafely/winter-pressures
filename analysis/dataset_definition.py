5b8110246ced040ab2933c4cd1112538810a4b1a
Iain Dillingham <iain@dillingham.me.uk>
2023-01-20 12:29:59 +0000

    Set relative to study period
    Here, we set the population relative to a study period (closed interval
    [1]), rather than to an index date; this study period encompasses the
    2019/2020 flu season. Rather than extracting the first 10 appointments
    that have a booked date on or after the start of the study period --
    appointments that could fall outside the study period -- we extract the
    first 10 appointments inside the study period.
    
    Closes #22
    
    [1]: https://en.wikipedia.org/wiki/Interval_(mathematics)

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -3,18 +3,19 @@ import datetime
 from databuilder import ehrql
 from databuilder.tables.beta.tpp import appointments, practice_registrations
 
-index_date = datetime.date(2020, 1, 1)
+start_date = datetime.date(2019, 7, 1)
+end_date = datetime.date(2020, 6, 30)
 
-# If a patient is registered at more than one practice on the index date, then return
+# If a patient is registered at more than one practice in the study period, then return
 # the registration with the most recent start date. If there are more than one
 # registrations with the most recent start date, then return the registration with the
 # longest duration.
-practice_registration_on_index_date = (
+practice_registration_in_study_period = (
     practice_registrations.take(
-        practice_registrations.start_date.is_on_or_before(index_date)
+        practice_registrations.start_date.is_on_or_before(end_date)
     )
     .take(
-        practice_registrations.end_date.is_on_or_after(index_date)
+        practice_registrations.end_date.is_on_or_after(start_date)
         | practice_registrations.end_date.is_null()
     )
     .sort_by(practice_registrations.start_date, practice_registrations.end_date)
@@ -27,24 +28,25 @@ dataset = ehrql.Dataset()
 # month for a period of 12 months, so changes to the population (e.g. patients entering
 # the system; patients leaving the system) were captured. Here, the dataset definition
 # is executed once, so changes to the population are not captured.
-dataset.set_population(practice_registration_on_index_date.exists_for_patient())
+dataset.set_population(practice_registration_in_study_period.exists_for_patient())
 
 # Administrative data
 # -------------------
-#
-# These data are extracted on the index date. However, the appointments data are
-# extracted on or after, and potentially a long time after, the index date.
-# Consequently, we may incorrectly attribute lead times to practices/regions.
+# These data are extracted for one practice. If a patient is registered at more than one
+# practice in the study period, then the appointments data *may* be incorrectly
+# attributed to the practice.
 
-dataset.practice = practice_registration_on_index_date.practice_pseudo_id
+dataset.practice = practice_registration_in_study_period.practice_pseudo_id
 
-dataset.region = practice_registration_on_index_date.practice_nuts1_region_name
+dataset.region = practice_registration_in_study_period.practice_nuts1_region_name
 
 # Appointments data
 # -----------------
 
-# The first appointment should have a booked date that is on or after the index date.
-apt = appointments.take(appointments.booked_date.is_on_or_after(index_date))
+# The first appointment should have a booked date in the study period.
+apt = appointments.take(appointments.booked_date.is_on_or_after(start_date)).take(
+    appointments.booked_date.is_on_or_before(end_date)
+)
 
 num_appointments = 10
 for i in range(1, num_appointments + 1):
@@ -59,5 +61,7 @@ for i in range(1, num_appointments + 1):
     setattr(dataset, f"lead_time_in_days_{i}", lead_time_in_days)
 
     # The next appointment should have a booked date that is after the booked date of
-    # the previous appointment.
-    apt = appointments.take(appointments.booked_date.is_after(apt.booked_date))
+    # the previous appointment, and in the study period.
+    apt = appointments.take(appointments.booked_date.is_after(apt.booked_date)).take(
+        appointments.booked_date.is_on_or_before(end_date)
+    )
