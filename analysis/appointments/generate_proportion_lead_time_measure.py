db63a018b3bb0400191eb711c4357dfeb40890fc
LisaHopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2023-02-13 08:28:16 +0000

    Improvements to proportion lead time measure calculation
    As suggested by @iaindillingham:
    (1) specifying int when parsing arguments
    (2) using booleans rather than ints (for memory reasons)

--- analysis/appointments/generate_proportion_lead_time_measure.py
+++ analysis/appointments/generate_proportion_lead_time_measure.py
@@ -27,7 +27,7 @@ def read(f_in, index_cols, value_col, date_col):
 def parse_args(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--value-col", required=True)
-    parser.add_argument("--value-threshold", required=True)
+    parser.add_argument("--value-threshold", required=True, type=int)
     parser.add_argument("--index-cols", action="extend", nargs="+", required=True)
     return parser.parse_args(args)
 
@@ -36,20 +36,17 @@ def main():
     args = parse_args(sys.argv[1:])
     # We assume the first column in the list of columns is a date column.
     date_col = args.index_cols[0]
-    value_threshold = int(args.value_threshold)
 
     f_in = OUTPUT_DIR / "dataset_long.csv.gz"
 
     dataset_long = read(f_in, args.index_cols, args.value_col, date_col)
-    dataset_long["threshold_mask"] = (
-        dataset_long[args.value_col] <= value_threshold
-    ).astype(int)
+    dataset_long["threshold_mask"] = ( dataset_long[args.value_col] <= args.value_threshold )
     total_counts = dataset_long.groupby(args.index_cols + ["threshold_mask"]).size()
     del dataset_long
 
     total_counts = total_counts.unstack("threshold_mask", fill_value=0)
     total_counts["population"] = total_counts.sum(axis=1)
-    measure = total_counts.reset_index().rename(columns={1: "value", date_col: "date"})
+    measure = total_counts.reset_index().rename(columns={True: "value", date_col: "date"})
     del total_counts
 
     measure = measure.loc[
@@ -58,7 +55,7 @@ def main():
 
     f_out = (
         OUTPUT_DIR
-        / f"measure_proportion_{args.value_col}_within{value_threshold}days_by_{date_col}.csv"
+        / f"measure_proportion_{args.value_col}_within{args.value_threshold}days_by_{date_col}.csv"
     )
     measure.to_csv(f_out, index=False)
 
