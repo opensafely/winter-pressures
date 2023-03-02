9b2a6e0c24fa033dd2fa849cf799e75b4ba3f216
LisaHopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2023-03-02 18:37:06 +0000

    Moving functions to utility script

--- analysis/classify_practices_by_decile.R
+++ analysis/classify_practices_by_decile.R
@@ -6,6 +6,8 @@ library(glue)
 library(stringr)
 library(here)
 
+source(here("analysis", "utils.R"))
+
 output_directory = here( "output", "combined" )
 output_file = "combined_seasonal_data.csv"
 
@@ -17,37 +19,6 @@ combined_data = paste(
     select( -X1 )
 
 combined_data_with_deciles = combined_data
-
-
-calculate_deciles = function( d, num_quantiles = 10 ) {
-    return( ntile( d, num_quantiles ))
-}
-
-calculate_seasonal_deciles_across_years_and_variables = function(data) {
-
-    data_long = data %>%
-        pivot_longer(
-            cols = starts_with("seasonal"),
-            names_to = "method",
-            values_to = "value"
-        )
-
-    data_with_deciles = data_long %>%
-        group_by(year, variable, method) %>%
-        mutate(decile = calculate_deciles(value))
-
-    return( data_with_deciles )
-}
-
-column_edit = function(s) {
-    new_s = s %>% str_remove("^value_")
-
-    if ( s %>% str_detect("^decile_" ) ) {
-        new_s = new_s %>% str_remove("^decile_")
-        new_s = glue( "{new_s}_decile" )
-    }
-    return( new_s )
-}
 
 combined_data_with_deciles = calculate_seasonal_deciles_across_years_and_variables(combined_data)
 
