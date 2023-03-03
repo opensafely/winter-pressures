0467756fe4eeb898a4cad97ca1f497e6267384c2
LisaHopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2023-03-03 09:05:15 +0000

    Creating normalisation function in `utils.R`

--- analysis/utils.R
+++ analysis/utils.R
@@ -1,3 +1,4 @@
+
 calculate_deciles = function( d, num_quantiles = 10 ) {
     return( ntile( d, num_quantiles ))
 }
@@ -26,4 +27,15 @@ column_edit = function(s) {
         new_s = glue( "{new_s}_decile" )
     }
     return( new_s )
+}
+
+normalise_raw_counts = function(raw_counts, practice_populations) {
+
+    counts_normalised <- raw_counts %>%
+        left_join(practice_populations, by = c("date", "practice")) %>%
+        mutate( value = raw_count / population ) %>%
+        select(value, date, practice)
+
+    return( counts_normalised )
+
 }
