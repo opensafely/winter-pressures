b950239021d9c00cb80c989e0a45e9495b25fa04
LisaHopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2023-03-07 12:40:32 +0000

    Adding tryCatch() to `seasonal_measures.R`
    An unfortunately quirk of the dummy data means that all practices are removed when filtering for practice size/population fluctuations. This change will capture errors/warnings from the filtering process and bundle them together to report at the end.

--- analysis/metrics/seasonal_measures.R
+++ analysis/metrics/seasonal_measures.R
@@ -7,12 +7,21 @@ library(here)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(glue)
 
 #######################################################################
 # source files
 #######################################################################
 
 source(here("analysis", "metrics","src", "generate_seasonal_metrics.R"))
+
+print_title = function(m) {
+  cat(paste(rep.int("=", 70), collapse = ""))
+  cat("\n")
+  cat(glue("Processing measure: {m}\n\n"))
+  cat(paste(rep.int("-", 70), collapse = ""))
+  cat("\n")
+}
 
 #######################################################################
 # read in aggregated SRO season data
@@ -26,9 +35,25 @@ sro_measure_name <- pull(
   sro_measure
 )
 
-invisible(lapply(sro_measure_name,
-                 create_seasonal_sro_plots)
-)
+for (m in sro_measure_name) {
+  print_title(m)
+  tryCatch(
+    expr ={ create_seasonal_sro_plots(m) },
+
+    error=function(cond) {
+      message(glue("There was an error creating seasonal measures for the {m} data"))
+      message(glue("[ERROR]>> {cond}"))
+    },
+
+    warning=function(cond) {
+      message(glue("There was a warning creating seasonal measures for the {m} data"))
+      message(glue("[WARNING]>> {cond}"))
+    },
+    
+    finally = {}
+  )
+}
+
 
 
 #######################################################################
@@ -43,10 +68,27 @@ kids_measure_name <- pull(
   kids_appt_measure
 )
 
+# invisible(lapply(kids_measure_name,
+#                  create_seasonal_kids_plots)
+# )
 
-invisible(lapply(kids_measure_name,
-                 create_seasonal_kids_plots)
-)
+for (m in kids_measure_name) {
+  print_title(m)
+  tryCatch(
+    expr = {
+      create_seasonal_kids_plots(m)
+    },
+    error = function(cond) {
+      message(glue("There was an error creating seasonal measures for the {m} data"))
+      message(glue("[ERROR]>> {cond}"))
+    },
+    warning = function(cond) {
+      message(glue("There was a warning creating seasonal measures for the {m} data"))
+      message(glue("[WARNING]>> {cond}"))
+    },
+    finally = {}
+  )
+}
 
 
 #######################################################################
@@ -61,6 +103,26 @@ appointment_measure_name <- pull(
   appointment_measure
 )
 
-invisible(lapply(appointment_measure_name,
-                 create_seasonal_appointment_plots)
-)
+
+for (m in appointment_measure_name) {
+  print_title(m)
+  tryCatch(
+    expr = {
+      create_seasonal_kids_plots(m)
+    },
+    error = function(cond) {
+      message(glue("There was an error creating seasonal measures for the {m} data"))
+      message(glue("[ERROR]>> {cond}"))
+    },
+    warning = function(cond) {
+      message(glue("There was a warning creating seasonal measures for the {m} data"))
+      message(glue("[WARNING]>> {cond}"))
+    },
+    finally = {}
+  )
+}
+
+# invisible(lapply(appointment_measure_name,
+#                  create_seasonal_appointment_plots)
+# )
+
