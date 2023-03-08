90f0997a874adbba32dbbf128487849d931b8b02
cewalters <c.e.walters6@gmail.com>
2023-03-08 13:44:43 +0000

    correct practices to remove
    - Remove unnecessary comments
    
    - Change population size threshold to 1000
    
    - Correct spelling mistake

--- analysis/metrics/identify_practices_to_remove.R
+++ analysis/metrics/identify_practices_to_remove.R
@@ -1,12 +1,3 @@
-rm(list = ls())
-#   for the SRO data,
-# I would load up  measure_asthma_practice_only_rate.csv  and select cols date, practice_id, population
-# I would load  measure_end_population_sro.csvand select cols date, practice_id, population_sro
-# I would join the data on date, practice_id
-# I would compare population and population_sro
-# Then repeat for all SRO measures.
-# Then apply similar logic to the appointments data. 
-
 #######################################################################
 # load libraries
 #######################################################################
@@ -28,7 +19,7 @@ source(here("analysis", "metrics","src", "practice_population_filtering.R"))
 #######################################################################
 
 percentage_threshold <- 10 # remove any practices that 
-population_size_threshold <- 500
+population_size_threshold <- 1000
 
 measure <- c("over12_appt_pop_rate",
              "under12_appt_pop_rate", 
@@ -36,12 +27,12 @@ measure <- c("over12_appt_pop_rate",
              "under12_appt_rate",
              "sro")
 
-list_practcies_to_remove <- lapply(measure, 
+list_practices_to_remove <- lapply(measure, 
        get_practices_to_remove,
        percentage_threshold = percentage_threshold,
        population_size_threshold = population_size_threshold)
 
-all_practices_to_remove <- unique(unlist(list_practcies_to_remove))
+all_practices_to_remove <- unique(unlist(list_practices_to_remove))
 all_practices_to_remove <- tibble(practice = all_practices_to_remove)
 
 print(paste0("Number of practices removed: ", nrow(all_practices_to_remove)))
