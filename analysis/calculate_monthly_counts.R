c449553f8c92d47f679aa440a88c0bac8ee9e0c6
LisaHopcroft <54442530+LisaHopcroft@users.noreply.github.com>
2023-03-15 08:08:35 +0000

    Calculating monthly totals
    This step will be helpful in:
    (1) comparing our data to national data
    (2) checking our seasonal summary calculations

---
+++ analysis/calculate_monthly_counts.R
@@ -0,0 +1,68 @@
+library(dplyr)
+library(readr)
+library(magrittr)
+library(glue)
+library(stringr)
+library(here)
+
+source(here("analysis", "utils.R"))
+
+# listsize_file = here("output", "listsize", "measure_listsize.csv")
+
+# listsizes = read_csv(listsize_file,
+#     col_types = cols_only(
+#         practice = col_integer(),
+#         population = col_double(),
+#         date = col_date()
+#     )
+# )
+
+target_dir = here("output", "appointments")
+target_pattern = "measure_monthly_num_.*.csv"
+
+# listsize_directory = here("output", "listsize")
+
+target_files = list.files(
+    path = target_dir,
+    pattern = target_pattern,
+    full.names = TRUE,
+    recursive = TRUE
+)
+
+output_dir = target_dir
+
+### Creating a directory to record which practices are not present
+### in either the raw counts or the population data
+check_directory <- fs::dir_create(
+    path = here("output", "check")
+)
+
+f_count = 0
+
+for (f in target_files) {
+    f_count = f_count + 1
+    
+    d = read_csv(f,
+        col_types = cols_only(
+            practice = col_integer(),
+            value = col_integer(),
+            date = col_date()
+        )
+    ) %>% rename( raw_count = value )
+
+    cat(glue("[{f_count}] Reading in '{basename(f)}'\n\n"))
+
+    ### == Recording the normalised counts ==========================
+    d_month_totals = d %>% group_by( date ) %>% summarise( total = sum(raw_count))
+    
+    output_file = paste(output_dir,
+        basename(f) %>% str_replace("num", "overall_num"),
+        sep = "/"
+    )
+    
+    cat(glue("[{f_count}] Writing out '{basename(output_file)}'\n\n"))
+
+    write.csv(d_month_totals, file = output_file, row.names=FALSE, quote=FALSE)
+
+
+}
