343965a3637469c3073563ff1218a32daff843c3
andrewscolm <73839417+andrewscolm@users.noreply.github.com>
2023-03-17 16:42:47 +0000

    add practice count to irr_data

---
+++ analysis/epi/practice_count.R
@@ -0,0 +1,26 @@
+## Import libraries ----
+library('tidyverse')
+library('lubridate')
+library('arrow')
+library('here')
+library('glue')
+library('purrr')
+
+
+deciles_n<-read_csv(here("output","combined","practice_counts_per_decile.csv")) %>%
+  filter(method == "seasonal_difference") %>%
+  summarise(metric = variable,
+            num_practices = num_practices)
+
+irr_data <- read_csv(here("output", "epi", "irr_data.csv"))
+
+# check no data is removed 
+irr_data_rows <- nrow(irr_data)
+
+# add deciles
+irr_data <- irr_data %>%
+  inner_join(deciles_n,"metric")
+
+print("number of rows dropped")
+print(irr_data_rows - nrow(irr_data))
+
