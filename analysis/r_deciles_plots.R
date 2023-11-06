f01b6964d828818754d32033bb806530a740fa74
cewalters <c.e.walters6@gmail.com>
2023-11-06 15:59:12 +0000

    Created a plotting function

--- analysis/r_deciles_plots.R
+++ analysis/r_deciles_plots.R
@@ -8,130 +8,112 @@ library(tidyverse)
 # read in data
 ####################################################################
 
-dat <- read_csv("output/deciles_table_monthly_median_lead_time_in_days_by_booked_month.csv") %>%
-  tibble() %>%
-  mutate(percentile = as.integer(percentile)) %>%
-  filter( percentile %in% seq(10, 90, by = 10)) %>%
-  filter(date <= as.Date("2022-05-01"))
+data_filepath <- c(
+  "output/deciles_table_monthly_median_lead_time_in_days_by_booked_month.csv",
+  "output/deciles_table_monthly_normalised_num_appointments_by_booked_month.csv",
+  "output/deciles_table_monthly_proportion_lead_time_in_days_within_0days_by_booked_month.csv",
+  "output/deciles_table_monthly_proportion_lead_time_in_days_within_2days_by_booked_month.csv",
+  "output/deciles_table_over12_appt_rate.csv",
+  "output/deciles_table_under12_appt_rate.csv"
+  )
 
-####################################################################
-# deciles plot
-####################################################################
+label <- c("Median lead time (days)",
+           "Average number of appointments per patient",
+           "Proportion of same day appointments",
+           "Proportion of appointments within 2 days",
+           "Appointment rate (12-15 year olds)",
+           "Appointment rate (5-12 year olds)"
+           )
 
-ggplot(data = dat,
-  aes(
-    x = date,
-    y = value,
-    group = percentile
-  )) +
-  geom_line(alpha = 1, 
-            colour = "#0000FF") +
-  theme_bw() +
-  scale_x_date(breaks="months") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
- 
-
-
-
-ggplot() +
-  geom_line(data = dat %>% filter(percentile == 50),
-            aes(
-              x = date,
-              y = value,
-              group = percentile
-            ),
-            alpha = 1, 
-            colour = "#0000FF",
-            size = 0.8) +
-  geom_line(data = dat %>% filter(percentile != 50),
-            aes(
-              x = date,
-              y = value,
-              group = percentile
-            ),
-            alpha = 0.5, 
-            colour = "#0000FF",
-            linetype = "dashed") +
-  theme_bw() +
-  scale_x_date(breaks="4 months") +
-  scale_y_continuous(breaks = seq(0, ceiling(max(dat$value)), by = 1)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-        axis.title.x =element_blank(),
-  ) +
-  ylab("median lead time (days)")
-
-
-
-
-plot_qof_deciles <- function(df,
-                             date_breaks = NULL,
-                             ylab = c("Prevalence", "% Achievement"),
-                             ylimits = c(0, 1),
-                             ybreaks = seq(0, 1, .25),
-                             scale_size_manual = c(.4, .4, .5, .6, .8, .6, .5, .4, .4),
-                             text_size = 14,
-                             legend_label = NULL,
-                             legend_position = "top",
-                             legend_top_nrow = 1,
-                             axis_x_text_size = 10,
-                             facet_wrap = TRUE,
-                             facet_wrap_var = indicator) {
-  plot <- df |>
-    ggplot(aes(
-      x = date,
-      y = value,
-      colour = percentile,
-      group = percentile,
-      size = percentile
-    )) +
-    geom_vline(
-      xintercept = lubridate::as_date(c(
-        "2019-03-01", "2020-03-01",
-        "2021-03-01", "2022-03-01"
-      )),
-      linetype = "dotted",
-      colour = "orange",
-      size = .7
-    ) +
-    geom_point(alpha = 1) +
-    geom_line(alpha = 1) +
-    scale_size_manual(values = scale_size_manual) +
-    scale_y_continuous(
-      labels = scales::label_percent(),
-      limits = ylimits,
-      breaks = ybreaks
-    ) +
-    scale_x_date(
-      breaks = date_breaks,
-      labels = scales::label_date_short()
-    ) +
-    labs(x = NULL, y = ylab, 
-         colour = legend_label, 
-         linetype = legend_label, 
-         size = legend_label) +
-    theme(legend.position = legend_position) +
-    scale_colour_manual(values = c(
-      "#9ecae1", "#6baed6", "#4292c6", "#2171b5",
-               "#084594",
-               "#2171b5", "#4292c6", "#6baed6", "#9ecae1"
-    )) +
-    theme(text = element_text(size = text_size), axis.text.x = element_text(size = axis_x_text_size))
+make_plots <- function(filepath,
+         y_axis_label){
   
-  if (legend_position == "top") {
-    plot <- plot + guides(
-      colour = guide_legend(nrow = legend_top_nrow),
-      group = guide_legend(nrow = legend_top_nrow),
-      size = guide_legend(nrow = legend_top_nrow),
-      linetype = guide_legend(nrow = legend_top_nrow)
-    )
-  }
+  dat <- read_csv(filepath) %>%
+    tibble() %>%
+    mutate(percentile = as.integer(percentile)) %>%
+    filter( percentile %in% seq(10, 90, by = 10)) %>%
+    filter(date <= as.Date("2022-05-01"))
   
-  if (facet_wrap) {
-    plot + facet_wrap(vars({{ facet_wrap_var }}), ncol = 3)
-  } else {
-    plot
-  }
+  ggplot() +
+    geom_line(data = dat %>% filter(percentile == 50),
+              aes(
+                x = date,
+                y = value,
+                group = percentile
+              ),
+              alpha = 1, 
+              colour = "#0000FF",
+              size = 0.8) +
+    geom_line(data = dat %>% filter(percentile != 50),
+              aes(
+                x = date,
+                y = value,
+                group = percentile
+              ),
+              alpha = 0.5, 
+              colour = "#0000FF",
+              linetype = "dashed") +
+    theme_bw() +
+    scale_x_date(breaks="4 months") +
+    #scale_y_continuous(breaks = seq(0, ceiling(max(dat$value)), by = 1)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+          axis.title.x =element_blank(),
+    ) +
+    ylab(y_axis_label)
+  
 }
 
 
-plot_qof_deciles(df = dat)
+# Median lead time
+
+make_plots(filepath = data_filepath[1],
+           y_axis_label = label[1]) + 
+  scale_y_continuous(breaks = seq(0, 
+                                  12, 
+                                  by = 1)
+  )
+
+# Average number of appointments per patient
+
+make_plots(filepath = data_filepath[2],
+           y_axis_label = label[2]) + 
+  scale_y_continuous(breaks = seq(0, 
+                                  1, 
+                                  by = 0.1)
+  )
+
+# Proportion of same day appointments
+
+make_plots(filepath = data_filepath[3],
+           y_axis_label = label[3]) + 
+  scale_y_continuous(breaks = seq(0, 
+                                  1, 
+                                  by = 0.1)
+  )
+
+# Proportion of appointments within 2 days
+
+make_plots(filepath = data_filepath[4],
+           y_axis_label = label[4]) + 
+  scale_y_continuous(breaks = seq(0, 
+                                  1, 
+                                  by = 0.1)
+  )
+
+# Appointment rate (12-15 year olds)
+
+make_plots(filepath = data_filepath[5],
+           y_axis_label = label[5]) + 
+  scale_y_continuous(breaks = seq(0, 
+                                  0.3, 
+                                  by = 0.05)
+  )
+
+# Appointment rate (5-12 year olds)
+
+make_plots(filepath = data_filepath[6],
+           y_axis_label = label[6]) + 
+  scale_y_continuous(breaks = seq(0, 
+                                  0.3, 
+                                  by = 0.05)
+  )
